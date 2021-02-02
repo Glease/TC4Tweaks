f91b3f7b2682246e81b5eca4c4603f9e98a796b0
Glease <4586901+Glease@users.noreply.github.com>
2021-02-02 14:38:30 +0800

    Prevent duplicate postponed update

--- src/main/java/net/glease/tc4tweak/asm/ASMCallhook.java
+++ src/main/java/net/glease/tc4tweak/asm/ASMCallhook.java
@@ -4,19 +4,18 @@ import net.glease.tc4tweak.ClientProxy;
 import thaumcraft.client.gui.GuiResearchTable;
 import thaumcraft.common.tiles.TileMagicWorkbench;
 
-import java.lang.ref.WeakReference;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
+import java.util.WeakHashMap;
 
 public class ASMCallhook {
-	private static final List<WeakReference<TileMagicWorkbench>> postponed = new ArrayList<>();
+	private static final WeakHashMap<TileMagicWorkbench, Void> postponed = new WeakHashMap<>();
 	// workbench throttling
 	private static long lastUpdate = 0;
 
 	public static void updatePostponed() {
 		synchronized (postponed) {
-			for (WeakReference<TileMagicWorkbench> workbench : postponed) {
-				TileMagicWorkbench tile = workbench.get();
+			for (Map.Entry<TileMagicWorkbench, Void> workbench : postponed.entrySet()) {
+				TileMagicWorkbench tile = workbench.getKey();
 				if (tile != null && tile.eventHandler != null && !tile.isInvalid() && tile.hasWorldObj()) {
 					// best effort guess on whether tile is valid
 					tile.eventHandler.onCraftMatrixChanged(tile);
@@ -50,7 +49,7 @@ public class ASMCallhook {
 			self.eventHandler.onCraftMatrixChanged(self); // 5 times per second at max
 		} else {
 			synchronized (postponed) {
-				postponed.add(new WeakReference<>(self));
+				postponed.put(self, null);
 			}
 		}
 	}
