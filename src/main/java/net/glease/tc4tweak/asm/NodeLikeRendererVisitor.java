551ea7a3d5073415dc674a05b4a281ec04b71e73
Glease <4586901+Glease@users.noreply.github.com>
2021-06-17 09:18:28 +0800

    Fix logging
    Signed-off-by: Glease <4586901+Glease@users.noreply.github.com>

--- src/main/java/net/glease/tc4tweak/asm/NodeLikeRendererVisitor.java
+++ src/main/java/net/glease/tc4tweak/asm/NodeLikeRendererVisitor.java
@@ -5,7 +5,6 @@ import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.MethodVisitor;
 
 import static net.glease.tc4tweak.asm.ASMConstants.ASMCALLHOOK_INTERNAL_NAME;
-import static thaumcraft.common.Thaumcraft.log;
 
 public class NodeLikeRendererVisitor extends ClassVisitor {
     private final String target;
@@ -18,7 +17,7 @@ public class NodeLikeRendererVisitor extends ClassVisitor {
         @Override
         public void visitMethodInsn(int opcode, String owner, String name, String desc, boolean itf) {
             if (owner.equals("thaumcraft/client/lib/UtilsFX") && name.equals("renderFacingStrip")) {
-                log.debug("Replacing renderFacingStrip");
+                TC4Transformer.log.debug("Replacing renderFacingStrip");
                 super.visitMethodInsn(opcode, ASMCALLHOOK_INTERNAL_NAME, name, desc, false);
             } else {
                 super.visitMethodInsn(opcode, owner, name, desc, itf);
@@ -38,7 +37,7 @@ public class NodeLikeRendererVisitor extends ClassVisitor {
     public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
         MethodVisitor mv = super.visitMethod(access, name, desc, signature, exceptions);
         if (name.equals(target)) {
-            log.debug("Visiting {}", target);
+            TC4Transformer.log.debug("Visiting {}", target);
             return new RenderNodeVisitor(api, mv);
         }
         return mv;
