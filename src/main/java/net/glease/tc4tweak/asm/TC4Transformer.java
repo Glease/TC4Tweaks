47a4a97533b68924dca26d577d2fc303fd896d60
Glease <4586901+Glease@users.noreply.github.com>
2021-09-05 22:50:56 +0800

    fix broken TESR NPE fix
    Signed-off-by: Glease <4586901+Glease@users.noreply.github.com>

--- src/main/java/net/glease/tc4tweak/asm/TC4Transformer.java
+++ src/main/java/net/glease/tc4tweak/asm/TC4Transformer.java
@@ -29,13 +29,13 @@ public class TC4Transformer implements IClassTransformer {
             .put("thaumcraft.client.gui.GuiResearchRecipe", new TransformerFactory(GuiResearchRecipeVisitor::new, Side.CLIENT))
             .put("thaumcraft.client.gui.GuiResearchTable", new TransformerFactory(GuiResearchTableVisitor::new, Side.CLIENT))
             .put("thaumcraft.client.gui.MappingThread", new TransformerFactory(MappingThreadVisitor::new, Side.CLIENT))
-            .put("thaumcraft.client.renderers.tile.TileAlchemyFurnaceAdvancedRenderer", TESRGetBlockTypeNullSafetyVisitor.FACTORY)
-            .put("thaumcraft.client.renderers.tile.TileChestHungryRenderer", TESRGetBlockTypeNullSafetyVisitor.FACTORY)
-            .put("thaumcraft.client.renderers.tile.TileEldritchCapRenderer", TESRGetBlockTypeNullSafetyVisitor.FACTORY)
-            .put("thaumcraft.client.renderers.tile.TileEldritchObeliskRenderer", TESRGetBlockTypeNullSafetyVisitor.FACTORY)
-            .put("thaumcraft.client.renderers.tile.TileManaPodRenderer", TESRGetBlockTypeNullSafetyVisitor.FACTORY)
-            .put("thaumcraft.client.renderers.tile.TileNodeConverterRenderer", TESRGetBlockTypeNullSafetyVisitor.FACTORY)
-            .put("thaumcraft.client.renderers.tile.TileNodeStabilizerRenderer", TESRGetBlockTypeNullSafetyVisitor.FACTORY)
+            .put("thaumcraft.client.renderers.tile.TileAlchemyFurnaceAdvancedRenderer", new TransformerFactory(TESRGetBlockTypeNullSafetyVisitor::new, Side.CLIENT, true))
+            .put("thaumcraft.client.renderers.tile.TileChestHungryRenderer", new TransformerFactory(TESRGetBlockTypeNullSafetyVisitor::new, Side.CLIENT, true))
+            .put("thaumcraft.client.renderers.tile.TileEldritchCapRenderer", new TransformerFactory(TESRGetBlockTypeNullSafetyVisitor::new, Side.CLIENT, true))
+            .put("thaumcraft.client.renderers.tile.TileEldritchObeliskRenderer", new TransformerFactory(TESRGetBlockTypeNullSafetyVisitor::new, Side.CLIENT, true))
+            .put("thaumcraft.client.renderers.tile.TileManaPodRenderer", new TransformerFactory(TESRGetBlockTypeNullSafetyVisitor::new, Side.CLIENT, true))
+            .put("thaumcraft.client.renderers.tile.TileNodeConverterRenderer", new TransformerFactory(TESRGetBlockTypeNullSafetyVisitor::new, Side.CLIENT, true))
+            .put("thaumcraft.client.renderers.tile.TileNodeStabilizerRenderer", new TransformerFactory(TESRGetBlockTypeNullSafetyVisitor::new, Side.CLIENT, true))
             .put("thaumcraft.client.renderers.tile.TileNodeRenderer", NodeLikeRendererVisitor.createFactory("renderNode"))
             .put("thaumcraft.client.renderers.tile.ItemNodeRenderer", new TransformerFactory(ItemNodeRendererVisitor::new, Side.CLIENT))
             .put("thaumcraft.common.tiles.TileMagicWorkbench", new TransformerFactory(TileMagicWorkbenchVisitor::new, Side.CLIENT))
@@ -53,6 +53,11 @@ public class TC4Transformer implements IClassTransformer {
             })
             .build();
 
+    static void catching(Exception e) {
+        log.fatal("Something went very wrong with class transforming! Aborting!!!", e);
+        throw new ReportedException(CrashReport.makeCrashReport(e, "Transforming class"));
+    }
+
     @Override
     public byte[] transform(String name, String transformedName, byte[] basicClass) {
         TransformerFactory factory = transformers.get(name);
@@ -63,35 +68,32 @@ public class TC4Transformer implements IClassTransformer {
         ClassReader cr = new ClassReader(basicClass);
         ClassWriter cw = new ClassWriter(factory.isExpandFrames() ? ClassWriter.COMPUTE_FRAMES : 0);
         // we are very probably the last one to run.
-        try {
-            cr.accept(factory.apply(ASM5, cw), (factory.isExpandFrames() ? ClassReader.SKIP_FRAMES : 0));
-        } catch (Exception e) {
-            Util.catching(e);
+        byte[] transformedBytes = null;
+        if (DEBUG) {
+            try (PrintWriter origOut = new PrintWriter(new File(debugOutputDir, name + "_orig.txt"), "UTF-8");
+                 PrintWriter tranOut = new PrintWriter(new File(debugOutputDir, name + "_tran.txt"), "UTF-8")) {
+                cr.accept(new TraceClassVisitor(factory.apply(ASM5, transformedName, new TraceClassVisitor(cw, tranOut)), origOut), factory.isExpandFrames() ? ClassReader.SKIP_FRAMES : 0);
+                transformedBytes = cw.toByteArray();
+            } catch (Exception e) {
+                log.warn("Unable to transform with debug output on. Now retrying without debug output.", e);
+            }
         }
-        byte[] transformedBytes = cw.toByteArray();
+        if (transformedBytes == null || transformedBytes.length == 0) {
+            try {
+                cr.accept(factory.apply(ASM5, transformedName, cw), factory.isExpandFrames() ? ClassReader.SKIP_FRAMES : 0);
+                transformedBytes = cw.toByteArray();
+            } catch (Exception e) {
+                catching(e);
+            }
+        }
         if (transformedBytes == null || transformedBytes.length == 0) {
             if (DEBUG) {
-                Util.catching(new RuntimeException("Null or empty byte array created. This will not work well!"));
+                catching(new RuntimeException("Null or empty byte array created. This will not work well!"));
             } else {
                 log.fatal("Null or empty byte array created. Transforming will rollback as a last effort attempt to make things work! However features will not function!");
                 return basicClass;
             }
-        } else {
-            if (DEBUG) {
-                try (PrintWriter pw = new PrintWriter(new File(debugOutputDir, name + ".txt"), "UTF-8")) {
-                    new ClassReader(transformedBytes).accept(new TraceClassVisitor(pw), 0);
-                } catch (Exception e) {
-                    log.warn("Unable to dump debug output!", e);
-                }
-            }
         }
         return transformedBytes;
-    }
-
-    private static class Util {
-        static void catching(Exception e) {
-            log.fatal("Something went very wrong with class transforming! Aborting!!!", e);
-            throw new ReportedException(CrashReport.makeCrashReport(e, "Transforming class"));
-        }
     }
 }
--- src/main/java/net/glease/tc4tweak/asm/TransformerFactory.java
+++ src/main/java/net/glease/tc4tweak/asm/TransformerFactory.java
@@ -4,23 +4,21 @@ import cpw.mods.fml.relauncher.FMLLaunchHandler;
 import cpw.mods.fml.relauncher.Side;
 import org.objectweb.asm.ClassVisitor;
 
-import java.util.function.BiFunction;
-
 class TransformerFactory {
-    private final BiFunction<Integer, ClassVisitor, ClassVisitor> factory;
+    private final TransformerProducer factory;
     private final Side activeSide;
     private final boolean expandFrames;
     private volatile ClassVisitor generated;
 
-    public TransformerFactory(BiFunction<Integer, ClassVisitor, ClassVisitor> factory) {
+    public TransformerFactory(TransformerProducer factory) {
         this(factory, null, false);
     }
 
-    public TransformerFactory(BiFunction<Integer, ClassVisitor, ClassVisitor> factory, boolean expandFrames) {
+    public TransformerFactory(TransformerProducer factory, boolean expandFrames) {
         this(factory, null, expandFrames);
     }
 
-    public TransformerFactory(BiFunction<Integer, ClassVisitor, ClassVisitor> factory, Side activeSide) {
+    public TransformerFactory(TransformerProducer factory, Side activeSide) {
         this(factory, activeSide, false);
     }
 
@@ -29,7 +27,30 @@ class TransformerFactory {
      * @param activeSide   the side this transformer will be active on. null for both side.
      * @param expandFrames whether the frames need to be recalculated
      */
-    public TransformerFactory(BiFunction<Integer, ClassVisitor, ClassVisitor> factory, Side activeSide, boolean expandFrames) {
+    public TransformerFactory(TransformerProducer factory, Side activeSide, boolean expandFrames) {
+        this.factory = factory;
+        this.activeSide = activeSide;
+        this.expandFrames = expandFrames;
+    }
+
+    public TransformerFactory(TransformerProducerSimple factory) {
+        this(factory, null, false);
+    }
+
+    public TransformerFactory(TransformerProducerSimple factory, boolean expandFrames) {
+        this(factory, null, expandFrames);
+    }
+
+    public TransformerFactory(TransformerProducerSimple factory, Side activeSide) {
+        this(factory, activeSide, false);
+    }
+
+    /**
+     * @param factory      the constructor of actual ClassVisitor. First argument is api level. Second argument is downstream ClassVisitor
+     * @param activeSide   the side this transformer will be active on. null for both side.
+     * @param expandFrames whether the frames need to be recalculated
+     */
+    public TransformerFactory(TransformerProducerSimple factory, Side activeSide, boolean expandFrames) {
         this.factory = factory;
         this.activeSide = activeSide;
         this.expandFrames = expandFrames;
@@ -39,11 +60,11 @@ class TransformerFactory {
         return activeSide != null && activeSide != FMLLaunchHandler.side();
     }
 
-    public final ClassVisitor apply(int api, ClassVisitor downstream) {
+    public final ClassVisitor apply(int api, String transformedName, ClassVisitor downstream) {
         if (generated == null) {
             synchronized (this) {
                 if (generated == null)
-                    generated = factory.apply(api, downstream);
+                    generated = factory.apply(api, transformedName, downstream);
             }
         }
         return generated;
@@ -51,5 +72,18 @@ class TransformerFactory {
 
     public boolean isExpandFrames() {
         return expandFrames;
+    }
+
+    interface TransformerProducer {
+        ClassVisitor apply(int api, String transformedName, ClassVisitor downstream);
+    }
+
+    interface TransformerProducerSimple extends TransformerProducer {
+        @Override
+        default ClassVisitor apply(int api, String transformedName, ClassVisitor downstream) {
+            return apply(api, downstream);
+        }
+
+        ClassVisitor apply(int api, ClassVisitor downstream);
     }
 }
