d1eed9c3617e59ee77cf7c05b8da9f1ab2b0041d
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2022-09-23 09:22:46 +0200

    prevent crash when infusion recipe instability is too high (#11)
    Co-authored-by: Glease <4586901+Glease@users.noreply.github.com>
    (cherry picked from commit 0434c1c2f8decac4c0e536186974489b7ae84028)

--- src/main/java/net/glease/tc4tweak/asm/TC4Transformer.java
+++ src/main/java/net/glease/tc4tweak/asm/TC4Transformer.java
@@ -59,6 +59,7 @@ public class TC4Transformer implements IClassTransformer {
             })
             .put("thaumcraft.common.lib.world.dim.CellLoc", new TransformerFactory(CellLocVisitor::new))
             .put("thaumcraft.common.lib.world.dim.MazeHandler", new TransformerFactory(MazeHandlerVisitor::new))
+            .put("thaumcraft.common.tiles.TileInfusionMatrix", new TransformerFactory(TileInfusionMatrixVisitor::new))
             .build();
 
     static void catching(Exception e) {
---
+++ src/main/java/net/glease/tc4tweak/asm/TileInfusionMatrixVisitor.java
@@ -0,0 +1,39 @@
+package net.glease.tc4tweak.asm;
+
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+public class TileInfusionMatrixVisitor extends ClassVisitor {
+
+    public TileInfusionMatrixVisitor(int api, ClassVisitor cv) {
+        super(api, cv);
+    }
+
+    @Override
+    public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+        final MethodVisitor mv = super.visitMethod(access, name, desc, signature, exceptions);
+        if (name.equals("craftCycle") && desc.equals("()V")) {
+            TC4Transformer.log.debug("Injecting Math#max before Random#nextInt calls in CraftCycle()");
+            return new LoadMathAbsVisitor(api, mv);
+        } else {
+            return mv;
+        }
+    }
+
+    private static class LoadMathAbsVisitor extends MethodVisitor {
+
+        public LoadMathAbsVisitor(int api, MethodVisitor mv) {
+            super(api, mv);
+        }
+
+        @Override
+        public void visitMethodInsn(int opcode, String owner, String name, String desc, boolean itf) {
+            if(opcode == Opcodes.INVOKEVIRTUAL && owner.equals("java/util/Random") && name.equals("nextInt") && desc.equals("(I)I") && !itf) {
+                mv.visitInsn(Opcodes.ICONST_1);
+                mv.visitMethodInsn(Opcodes.INVOKESTATIC, "java/lang/Math", "max", "(II)I", false);
+            }
+            super.visitMethodInsn(opcode, owner, name, desc, itf);
+        }
+    }
+}
